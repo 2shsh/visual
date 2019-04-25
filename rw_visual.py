cf360ec4ced7aca637f4ce44734c0edce840f5fb
2shsh <kashshapov@gmail.com>
2019-04-25 22:51:42 +0500

    Class generate random walk

--- rw_visual.py
+++ rw_visual.py
@@ -2,4 +2,17 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
+# Новые блуждания строятся до тех пор, пока программа остаётся активной.
+while True:
+    # Построение случайного блуждания и нанесение точек на диаграмму.
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolors='none' ,s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
 
